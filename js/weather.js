72e9edc2fe22b5dcf85ced855e7948750781a95d
eternity <hsl2335597819@163.com>
2023-01-10 16:11:56 +0800

    Site updated: 2023-01-10 16:11:37

---
+++ js/weather.js
@@ -0,0 +1,1 @@
+WIDGET={CONFIG:{modules:"01234",background:"1",tmpColor:"FFFFFF",tmpSize:"16",cityColor:"FFFFFF",citySize:"16",aqiColor:"FFFFFF",aqiSize:"16",weatherIconSize:"24",alertIconSize:"18",padding:"10px 10px 10px 10px",shadow:"0",language:"auto",borderRadius:"5",fixed:"false",vertical:"top",horizontal:"left",key:"5c1acd034a4048298f74e784df3b2f50"}};
